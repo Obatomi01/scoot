09d060994b5d28db90d2c36739c55a83b90aaed4
0batomi01 <eniolaoba9@gmail.com>
2022-11-10 10:38:23 +0100

    update

--- pages/about/questionSection.js
+++ pages/about/questionSection.js
@@ -7,7 +7,7 @@ import styles from "../../styles/about/faqs.module.css";
 const QuestionSection = (props) => {
   // SOLVED✅ i used the array QA as the initial state for the Q&A. I added a key "showAnswer" which will be changed periodically once the particular item with the corresponding id is clicked.
 
-  const [qa, setQA] = useState((props.questionArr = []));
+  const [qa, setQA] = useState(props.questionArr);
 
   const showAnswerHowItWorksHandler = (id = null) => {
     // SOLVED✅ this function accepts a parameter id. And a new Array is formed from the initial "showAnswerHowItWorks" so that the id of the item of the initial "showAnswerHowItWorks" that matches with the id of the item clicked is checked and the "showAnswer" value is then changed
